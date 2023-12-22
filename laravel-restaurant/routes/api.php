a71ec5682f06d422e5ab972a33e94697045e16fa
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-22 16:12:25 -0300

    productos y paginacion

--- laravel-restaurant/routes/api.php
+++ laravel-restaurant/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::apiResource('/categorias', CategoriaController::class);
+Route::apiResource('/productos', ProductoController::class);
+
 
