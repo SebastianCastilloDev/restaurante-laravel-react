ab10f4f5b513c92a22730025e4bf182a526ebac2
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-23 13:23:09 -0300

    habilitando cors

--- laravel-restaurant/app/Http/Controllers/AuthController.php
+++ laravel-restaurant/app/Http/Controllers/AuthController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(RegistroRequest $request) {
         // validar el registro
-        $data = $request->validate();
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        //devuelve una respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
         
     }
     public function login(Request $request) {
