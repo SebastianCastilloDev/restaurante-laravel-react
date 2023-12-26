82f8601ef03b66eae49e727f20fbac513f99557f
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-26 01:46:19 -0300

    Mostrando el contenido del pedido

--- laravel-restaurant/app/Models/Pedido.php
+++ laravel-restaurant/app/Models/Pedido.php
@@ -13,6 +13,6 @@ class Pedido extends Model
     }
 
     public function productos() {
-        return $this->belongsToMany(Producto::class, 'pedido_productos');
+        return $this->belongsToMany(Producto::class, 'pedido_productos')->withPivot('cantidad');
     }
 }
--- react-restaurant/src/layouts/Ordenes.jsx
+++ react-restaurant/src/layouts/Ordenes.jsx
@@ -9,11 +9,9 @@ export default function Ordenes() {
         }
     })
 
-    const { data, error, isLoading } = useSWR('/api/pedidos', fetcher, { refreshInterval: 1000 })
+    const { data, error, isLoading } = useSWR('/api/pedidos', fetcher)
 
-    console.log(data)
-    console.log(error)
-    console.log(isLoading)
+    if (isLoading) return 'Cargando...';
 
 
     return (
@@ -22,6 +20,27 @@ export default function Ordenes() {
             <p className='text-2xl my-10'>
                 Administra las ordenes desde aquí
             </p>
+            <div>
+                {data.data.data.map(pedido => (
+                    <div key={pedido.id} className='p-5 bg-white shadow space-y-2 border-b'>
+                        <p className="text-xl font-bold text-slate-600">
+                            Contenido del pedido:
+                        </p>
+                        {pedido.productos.map(producto => (
+                            <div key={producto.id} className='border-b border-b-slate-200 last-of-type:border-none py-4'>
+                                <p className='text-sm'>ID: {producto.id}</p>
+                                <p>{producto.nombre}</p>
+                                <p>
+                                    Cantidad: {''}
+                                    <span className='font-bold'>{producto.pivot.cantidad}</span>
+                                </p>
+                            </div>
+                        ))}
+
+
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
