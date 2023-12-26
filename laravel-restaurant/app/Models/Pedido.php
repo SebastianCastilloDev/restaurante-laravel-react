21a6e79e29504b69dbbaa841139862bcb8d2fdee
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-26 01:34:37 -0300

    Añadir los productos del pedido en el JSON

--- laravel-restaurant/app/Models/Pedido.php
+++ laravel-restaurant/app/Models/Pedido.php
@@ -11,4 +11,8 @@ class Pedido extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function productos() {
+        return $this->belongsToMany(Producto::class, 'pedido_productos');
+    }
 }
